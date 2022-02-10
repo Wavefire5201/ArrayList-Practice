e13e65bbdd17692b30910b4ca4af51c08dc92be5
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2022-02-09 22:55:55 -0600

    Update Factors.java

--- Factors.java
+++ Factors.java
@@ -5,33 +5,42 @@ public class Factors {
         ArrayList<Integer> array = new ArrayList<Integer>();
 
         for (int i = 2; i < number; i++ ) {
-            
+            if (number % i == 0) {
+                array.add(i);
+            }
         }
-
-
+        
         return array;
     }
-    public static ArrayList<Integer> keepOnlyCompositeNumbers( ArrayList<Integer> nums )
+
+    public static void keepOnlyCompositeNumbers( ArrayList<Integer> nums )
     {
         ArrayList<Integer> array = nums;
 
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.get(i); j++) {
-                // if j
+            int counter = 0;
+            for (int j = 2; j < nums.get(i); j++) {
+                if (nums.get(i) % i == 0) {
+                    counter++;
+                }
+                
+            }
+            if (counter > 2) {
+                array.remove(nums.get(i));
             }
         }
 
-        return array;
+        System.out.println(array);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> nums = new ArrayList<Integer>();
         System.out.print("Please enter an integer >>> ");
+        int userInput = in.nextInt();
 
-        nums.add(in.nextInt());
-        
-        System.out.println(keepOnlyCompositeNumbers(nums));
+        ArrayList<Integer> factorList = getListOfFactors(userInput); 
+        System.out.println(factorList);
+        keepOnlyCompositeNumbers(factorList);
         in.close();
     }
 }
