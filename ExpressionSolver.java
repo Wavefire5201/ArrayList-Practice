e72197881cf6de2a1ca6dc4b8ce61c07ee4e732b
Wavefire5201 <70147423+Wavefire5201@users.noreply.github.com>
2022-03-27 16:41:54 -0500

    yea

--- ExpressionSolver.java
+++ ExpressionSolver.java
@@ -2,20 +2,33 @@ import java.util.*;
 public class ExpressionSolver {
 
     public static double advanced(ArrayList<String> equation) {
-        while (equation.contains("*")) {
-            int x = equation.indexOf("*");
-            double multi = Double.valueOf(equation.get(x - 1)) * Double.valueOf(equation.get(x + 1));
-            equation.remove(x + 1);
-            equation.set(x, String.valueOf(multi));
-            equation.remove(x - 1);  
-        }
+        while (equation.contains("*") || equation.contains("/")) {
+            int multi_ = equation.indexOf("*");
+            int divi_ = equation.indexOf("/");
+            if (multi_ < divi_ || (divi_ == -1)) {
+                    if (multi_ == -1) {
+                        break;
+                    }
+                // while (equation.contains("*")) {
+                    int x = equation.indexOf("*");
+                    double multi = Double.valueOf(equation.get(x - 1)) * Double.valueOf(equation.get(x + 1));
+                    equation.set(x, String.valueOf(multi));
+                    equation.remove(x + 1);
+                    equation.remove(x - 1);  
+               // }
+            } else {
+                    if (divi_ == -1) {
+                        break;
+                    }
+                //while (equation.contains("/")) {
+                    int x = equation.indexOf("/");
+                    double divi = Double.valueOf(equation.get(x - 1)) / Double.valueOf(equation.get(x + 1));
+                    equation.set(x, String.valueOf(divi));
+                    equation.remove(x + 1);
+                    equation.remove(x - 1);
+                //}
+            }
 
-        while (equation.contains("/")) {
-            int x = equation.indexOf("/");
-            double divi = Double.valueOf(equation.get(x - 1)) / Double.valueOf(equation.get(x + 1));
-            equation.remove(x + 1);
-            equation.set(x, String.valueOf(divi));
-            equation.remove(x - 1);
         }
 
         double sum = Double.valueOf(equation.get(0));
--- MazeSolver/MazeSolver.java
+++ MazeSolver/MazeSolver.java
@@ -2,29 +2,85 @@ import java.util.*;
 import java.io.*;
 public class MazeSolver {
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner( new File("TESTMAZE.txt"));
-
+        Scanner in = new Scanner( new File("TESTMAZE2.txt"));
         ArrayList<Point> points = new ArrayList<Point>();
         int row = 0;
         Point exit = null;
         while (in.hasNext()) {
             String q = in.nextLine();
-            System.out.println(q);
             for (int col = 0; col < q.length(); col++) {
-                if (String.valueOf(q.charAt(col)) == " ") {
+                if (q.charAt(col) == ' ') {
                     points.add(new Point(row, col));
-                } else if (String.valueOf(q.charAt(col)) == "S") {
+                } else if (q.charAt(col) == 'S') {
                     points.add(0, new Point(row, col));
-                } else if (String.valueOf(q.charAt(col)) == "$") {
+                } else if (q.charAt(col) == '$') {
                     exit = new Point(row, col);
                 }
             }
             row++;
         }
         points.add(exit);
-        System.out.println(points.size());
-
         ArrayList<Point> second = new ArrayList<Point>();
+        second.add(points.get(0));
+        points.remove(0);
+        while (points.size() > 0) {
+            if (second.size() == 0) {
+                System.out.println("Maze is not solvable.");
+                break;
+            }
+            if (second.get(0).isAdjacent(points.get(points.size() - 1)) && second.size() != 0) {
+                System.out.println("Maze is solved!");
+                break;
+            }
+            
+            for (int i = 0; i < points.size(); i++) {
+                if (second.get(0).isAdjacent(points.get(i))) {
+                    second.add(points.get(i));
+                    points.remove(i);
+                    if (points.size() == 0) {
+                        System.out.println("Maze is solved!");
+                        break;
+                    }
+                }
+            }
+            if (second.size() > 0) {
+                second.remove(0);
+            } else if (second.size() == 0) {
+                System.out.println("Maze is not solvable.");
+            }
+        }
+        // while (points.size() > 0) {
+        //     if (second.size() == 0) {
+        //         System.out.println("Maze is not solvable.");
+        //         break;
+        //     }
+            
+        //     for (int i = 0; i < points.size(); i++) {
+        //         if (second.size() == 0) {
+        //             System.out.println("Maze is not solvable.");
+        //             break;
+        //         }
+        //         Point p = second.get(0);
+        //         if (p.isAdjacent(exit)) {
+        //             System.out.println("Maze is solved!");
+        //             break;
+        //         }
+        //         if (p.isAdjacent(points.get(i))) {
+        //             second.add(points.get(i));
+        //             points.remove(i);
+        //             System.out.println("removed");
+        //             if (points.size() == 0) {
+        //                 System.out.println("Maze is solved.");
+        //                 break;
+        //             }
+        //         }
+        //         if (second.size() > 0) {
+        //             second.remove(0);
+        //         }
+        //     }
+        // }
+        // System.out.println(points.size());
+        // System.out.println(second.size());
         // for (Point q:points) {
         //     System.out.println(q.toString());
         // }
